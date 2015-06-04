f8cb21d7b321addc5e60e259458c2cc72efa32d3
Nuno Valente <nuno.av@live.com.pt>
2015-06-04 19:13:22 +0100

    Melhorias no aspeto grafico do carousel

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -72,13 +72,13 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-12">
-                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <div class="col-lg-10 col-centered">
+                <div id="carosel-landing-page" class="carousel slide carousel-border" data-ride="carousel">
                     <!-- Indicators -->
                     <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        <li data-target="#carosel-landing-page" data-slide-to="0" class="active"></li>
+                        <li data-target="#carosel-landing-page" data-slide-to="1"></li>
+                        <li data-target="#carosel-landing-page" data-slide-to="2"></li>
                     </ol>
     
                     <!-- Wrapper for slides -->
@@ -102,16 +102,16 @@
                             </div>
                         </div>
                     </div>
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carosel-landing-page" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carosel-landing-page" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
         </div>
     </div>
