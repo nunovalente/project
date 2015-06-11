632d2f5d51f799d72430168380f076d8bd140955
Nuno Valente <nuno.av@live.com.pt>
2015-06-11 15:21:25 +0100

    repeat

--- resources/views/projects/projectbrowser.blade.php
+++ resources/views/projects/projectbrowser.blade.php
@@ -169,7 +169,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; IPL Project Browser</p>
                 </div>
             </div>
             <!-- /.row -->
